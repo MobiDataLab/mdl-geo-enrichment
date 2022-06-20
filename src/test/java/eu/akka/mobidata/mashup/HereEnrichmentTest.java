3bbc2da311d9389c22a95bdb316221f573435940
KARAMI Mohamed <karamimed@gmail.com>
2022-06-20 15:50:13 +0200

    [AKRMDL-1163] lookup smaller area box for here's stations api

--- src/test/java/eu/akka/mobidata/mashup/HereEnrichmentTest.java
+++ src/test/java/eu/akka/mobidata/mashup/HereEnrichmentTest.java
@@ -33,7 +33,7 @@ public class HereEnrichmentTest {
 
     private final String HERE_ROUTES_REQ = "api/v1/here/getRoutes?apiKey=0PMpb1W_5iihYGu7UrBWsR8fI6Utopf52hFBKOwl7Xc&apiFormat=GeoJson&apiUrl=https://www.overpass-api.de/api/interpreter?data=[out:json];node[highway=bus_stop](48.8345631,2.2433581,48.8775033,2.4400646);out%20meta;&enrichAttributes=wheelchair, shelter, tactile_paving, bench, bin, lit&fromCoordinates=48.8345631,2.2433581&toCoordinates=48.8775033,2.4400646";
 
-    private final String HERE_STATIONS_REQ = "api/v1/here/getNearStations?apiFormat=GeoJson&apiKey=0PMpb1W_5iihYGu7UrBWsR8fI6Utopf52hFBKOwl7Xc&apiUrl=https://www.overpass-api.de/api/interpreter?data=[out:json];node[highway](48.856892, 2.332623,48.896892, 2.372623);node[railway](48.856892, 2.332623,48.896892, 2.372623);out%20meta;&enrichAttributes=wheelchair, shelter, tactile_paving, bench, bin, lit&coordinates=48.8545631,2.3433581";
+    private final String HERE_STATIONS_REQ = "api/v1/here/getNearStations?apiFormat=GeoJson&apiKey=0PMpb1W_5iihYGu7UrBWsR8fI6Utopf52hFBKOwl7Xc&apiUrl=https://www.overpass-api.de/api/interpreter?data=[out:json];node[highway](48.856892, 2.332623,48.876892, 2.352623);node[railway](48.856892, 2.332623,48.876892, 2.352623);out%20meta;&enrichAttributes=wheelchair, shelter, tactile_paving, bench, bin, lit&coordinates=48.8545631,2.3433581";
 
     @DisplayName("Test here station enrichment")
     @Test
